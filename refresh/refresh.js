d1493933a8de1f75cc183bdb4853efe279bea52b
miguel <mguardos@gmail.com>
2016-07-18 20:01:25 -0700

    [master] setting up refresh rules for both SW and AC

---
+++ refresh/refresh.js
@@ -0,0 +1,109 @@
+// Version to debug cache issues :: V1
+
+// Debugging message to display messages during multi-browser testing
+function showMsg(message, dyn){
+  if (!dyn)
+      dyn = '';
+  // Check whether to display messages via console or window alert
+  if ((document.querySelector('input[name=popupAlerts]').checked) || 
+    (localStorage.getItem('alerts')))
+    window.alert(message + ' - ' + dyn);
+  else
+    console.log(message, dyn);
+}
+
+// Create service worker
+if ( !'serviceWorker' in navigator ) {
+  // $$$$$ Change path and location for sw.js to propefly cache common files from the root
+  navigator.serviceWorker.register( 'sw-refresh.js' ).then( function( registration ) {
+    // Registration was successful
+    showMsg( 'ServiceWorker registration successful with scope: ', registration.scope );
+    if ( registration.installing ) {
+      showMsg( 'Service worker installing' );
+    } else if ( registration.waiting ) {
+      showMsg( 'Service worker installed' );
+    } else if ( registration.active ) {
+      showMsg( 'Service worker active' );
+    }
+  } ).catch( function( err ) {
+    // registration failed :(
+    showMsg( 'ServiceWorker registration failed: ', err );
+  } );
+    // Handler for messages coming from the service worker
+  navigator.serviceWorker.addEventListener('message', function(event){
+    // newVersion flag will be set true for each new SW activation
+    if (event.data.newVersion)
+      reloadPage('SW');
+  });
+
+
+// Check browser storage abuser $$$$
+// demo.agektmr.com/storage
+
+  // AppCache management
+} else if ('applicationCache' in window) {
+  // Load iframe with appCache
+  // As suggested by both Jake Archibald and Patrick Kettner
+  var iframe = document.createElement('iframe');
+  iframe.style.display = 'none';
+  iframe.src = 'load-appcache.html'
+  document.body.appendChild(iframe);
+  showMsg('Iframe loaded for AppCache management');
+
+  // // Check if a new cache is available on page load.
+  // window.addEventListener('load', function(e) {
+
+  //   window.applicationCache.addEventListener('updateready', function(e) {
+  //     if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+  //       // Browser downloaded a new app cache.
+  //       if (confirm('A new version of this site is available. Load it?')) {
+  //         window.location.reload();
+  //       }
+  //     } else {
+  //       // Manifest didn't changed. Nothing new to server.
+  //     }
+  //   }, false);
+
+  // }, false);
+
+} else {
+  showMsg('no service worker - no appCache');
+}
+
+  // Upon SW update, the application refreshes twice:
+  // First one to update SW file and read other files, second to push new files to browser
+  // We want to clean variables only after the final refresh
+// Update interrupted inputs in case of a forced update
+if (localStorage.getItem('updExample')) {
+  document.querySelector('input[name=updExample]').value = localStorage.getItem('updExample');
+}
+
+
+function reloadPage(label){
+  showMsg('about to reload page...');
+  top.location.reload();
+}
+
+
+function checkUpdates(){
+  showMsg('Checking updates ...');
+  // This logic would be called whenever an update service returns that the current version is outdated
+  // simulated by the screen input
+  if (document.querySelector('#updatedApp').checked) {
+    window.alert('There is a new version of the application. It will be reloaded and your changes will be saved');
+    saveEntriesTemp();    
+    reloadPage();
+  }
+
+}
+
+function showMsgFlag(){
+  if (document.querySelector('input[name=popupAlerts]').checked) 
+    localStorage.setItem('alerts', true);
+  else
+    localStorage.removeItem('alerts');
+}
+
+function saveEntriesTemp(){
+  localStorage.setItem('updExample', document.querySelector('input[name=updExample]').value);
+}
---
+++ refresh/sw-refresh.js
@@ -0,0 +1,89 @@
+// The files we want to cache
+const version = '31';
+
+var CACHE_NAME = 'refresh-' + version;
+var urlsToCache = [
+  '.',
+  'index.html',
+  'refresh.js',
+  'refresh.css',
+];
+
+// Warning. Service Workers do not have access to the DOM, so window.alert is not allowed
+
+// Set the callback for the install step
+self.addEventListener( 'install', function( event ){
+    // Perform install steps
+    event.waitUntil(
+    caches.open( CACHE_NAME )
+      .then( function( cache ) {
+        console.log( 'Opened cache: ', CACHE_NAME );
+        return cache.addAll( urlsToCache );
+      })
+      // The following call will activate the new Service Worker immediately
+      // This seems obvious because if a new SW is available, it means there is connectivity and old cached files can be replaced
+      .then( function( ){
+        self.skipWaiting( );
+      })
+      .catch( function( e ){
+        console.log("Error skipWaiting", e);
+      })
+  );        
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log("fetched request: ", event.request);    
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+      	console.log("caching match: ", response);
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .catch(function(){
+            return caches.match('offline.jpg');
+            }
+          );
+          // .then(
+          //   function(response){
+          //     // Do something
+          //   }
+          // );
+      }
+    )
+  );
+});
+
+self.addEventListener( 'activate', function( event ){
+  var cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys( )
+      .then( function( keyList ){
+        return Promise.all( keyList.map( function( key ){
+          if ( cacheWhitelist.indexOf( key ) === -1 ){
+            console.log( "Deleting cache: ", CACHE_NAME, key );
+            return caches.delete(key);
+          }
+        }));
+      })
+      .then( function( ){
+        self.clients.claim( )
+      })
+      .then(function() {
+        // After the activation and claiming is complete, send a message to each of the controlled
+        // pages letting it know that it's active.
+        // This will trigger navigator.serviceWorker.onmessage in each client.
+        return self.clients.matchAll().then(function(clients) {
+          return Promise.all(clients.map(function(client) {
+            return client.postMessage({'newVersion':true});
+          }));
+        });
+      })        
+      .catch( function( ){
+        console.log("error during activation");
+      })
+  );
+});
